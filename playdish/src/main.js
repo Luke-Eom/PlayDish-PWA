f10f7f59ccee5e1fc9b74494d5de841f9bb6e488
LukeEom <eluke1028@gmail.com>
2023-01-19 01:18:25 +0900

    add vuetify
    ! error occurred:
    Error: You cannot call "get" on a collection with no paths. Instead, check the "length" property first to verify at least 1 path exists.
    
    A: solved by adding lines in main.js
    new Vue({
        render: h => h(App),
    }).$mount('#app');
    
    environment: Vue 3, Vuetify 2 Vue CLI

--- playdish/src/main.js
+++ playdish/src/main.js
@@ -2,5 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
+import vuetify from './plugins/vuetify'
+
+new Vue({
+    vuetify,
+    render: h => h(App)
+}).$mount('#app');
 
 createApp(App).use(router).mount('#app')
